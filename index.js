100284531de1290da3fb734cd75265a8db013840
veenaanappalli123 <veena2006may@gmail.com>
2025-11-14 21:06:20 +0100

    Initial project setup

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello from Node!")
+const userOne = {
+  firstName: "Bog",
+  age: 27,
+  admin: false
+}
+
+userOne.age = 30
+console.log(userOne)
+
+const sports = ["tennis", "golf", "volleyball"]
+console.log(sports)
